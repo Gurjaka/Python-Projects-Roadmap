85a11ea157ab8bcff4bf93efd56ca67e2312ec48
Gurjaka <esartia.gurika@gmail.com>
2025-02-25 22:44:51 +0400

    flake rewrite

--- flake.nix
+++ flake.nix
@@ -1,34 +1,43 @@
 {
-  description = "Weather Cli Project";
+  description = "Python Projects Roadmap";
 
   inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      py = pkgs.python3.pkgs;
-  in
-  {
-    devShells.${system}.default =
-    pkgs.mkShell
-      {
-      packages =  with pkgs; [
-        python3
-        zsh 
-        py.pip
-        py.requests
-        py.simplejson
-        py.flask
-        py.playsound
-        py.beautifulsoup4
-      ];
-      
-      shellHook = ''
-        exec zsh 
-      '';
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs supportedSystems (
+        system: function (import nixpkgs {inherit system;})
+      );
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+
+    devShells = forAllSystems (
+      pkgs: let
+        python-deps = ps:
+          with ps; [
+            py.pip
+            py.requests
+            py.simplejson
+            py.flask
+            py.playsound
+            py.beautifulsoup4
+          ];
+      in {
+        default =
+          pkgs.mkShell
+          {
+            packages = with pkgs; [
+              (python3.withPackages python-deps)
+              black
+            ];
+          };
+      }
+    );
   };
 }
